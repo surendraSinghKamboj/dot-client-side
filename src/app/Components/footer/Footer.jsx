a318328f79dd154a6bc4a64b783a64c84be5c9c8
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-06-19 18:26:35 +0530

    Image logo fixed

--- src/app/Components/footer/Footer.jsx
+++ src/app/Components/footer/Footer.jsx
@@ -2,13 +2,14 @@
 import React from 'react'
 import './Footer.css';
 import logoImg from './user.png';
+import Image from "next/image";
 // import HiOfficeBuilding from 'react-icons/hi';
 const Footer = () => {
   return (
     <div className='parent'>
         <div className='Container'>
               <div className='ComapnySection sub-container'>
-                   {/* <img src={logoImg} alt="Logo Img" /> */}
+                    <Image src={logoImg} alt="Logo Img" />
                    <h1 className='logo'>LOGO</h1>
                    <p className='aboutCompany'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit blanditiis, illo vel quidem quod earum dolore eum voluptas </p>
               </div>
